899107191fdb50f52ee082f5b27dc0e485af9bf6
AhmadMasry <good.dr.ahmad@gmail.com>
2022-07-02 01:38:25 +0300

    fix index path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp /deploy/index.html /var/www/html/index.html
 chown apache:apache /var/www/html/index.html
-echo "This is machine $(hostname -I)" >> /var/www/html/index.html
+echo "\nThis is machine $(hostname -I)" >> /var/www/html/index.html
