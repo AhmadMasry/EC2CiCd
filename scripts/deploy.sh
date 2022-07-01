97e470065ed3dea5b1e11efbe19bd9931b7e53dc
AhmadMasry <good.dr.ahmad@gmail.com>
2022-07-02 01:42:09 +0300

    fix format

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cp /deploy/index.html /var/www/html/index.html
 chown apache:apache /var/www/html/index.html
-echo "" >> /var/www/html/index.html
-echo "This is machine $(hostname -I)" >> /var/www/html/index.html
+echo "<p>This is machine $(hostname -I)<br></p>" >> /var/www/html/index.html
